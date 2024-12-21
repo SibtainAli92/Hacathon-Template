01a9b03288cfa60ec081dcaa5da1b135fe43167f
Sibtain Ali <hellofozi@gmail.com>
2024-12-21 16:53:57 +0500

    comment in next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {
-  reactStrictMode: true,
-  distDir: 'build', 
-};
+// /** @type {import('next').NextConfig} */
+// // const nextConfig: import('next').NextConfig = {
+// //   reactStrictMode: true,
+// //   distDir: 'build', 
+// // };
 
-module.exports = nextConfig;
+// // module.exports = nextConfig;
+
+// const nextConfig = {}
+// export default nextConfig
